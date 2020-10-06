a3fbd5d05e427dc4b25762ee1b7ea2c35e4296c3
Onodera Sho <onodera@haselab.net>
2020-10-07 00:04:21 +0900

    show graph clearly

--- app/src/main/java/com/onodera/BleApp/template/signal/SendingSignalActivity.java
+++ app/src/main/java/com/onodera/BleApp/template/signal/SendingSignalActivity.java
@@ -2,7 +2,13 @@ package com.onodera.BleApp.template.signal;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
 import android.graphics.DashPathEffect;
 import android.os.Bundle;
@@ -10,6 +16,7 @@ import android.view.MenuItem;
 import android.view.View;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
@@ -17,6 +24,7 @@ import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.onodera.BleApp.R;
+import com.onodera.BleApp.template.TemplateService;
 
 import java.util.ArrayList;
 
@@ -24,6 +32,34 @@ import java.util.ArrayList;
 public class SendingSignalActivity extends AppCompatActivity {
 
     private LineChart mChart;
+
+    //private int[] intValue = new int[20];
+    //private int index = 0;
+
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(final Context context, final Intent intent) {
+            final String action = intent.getAction();
+            final BluetoothDevice device = intent.getParcelableExtra(TemplateService.EXTRA_DEVICE);
+            if (TemplateService.BROADCAST_TEMPLATE_MEASUREMENT.equals(action)) {
+                byte[] value = intent.getByteArrayExtra(TemplateService.EXTRA_DATA);
+                // Update GUI
+                //int[] intValue = new int[20];
+                int intValue;
+                for(int i=0; i<20; i++){
+                    intValue = value[i] & 0xFF;
+                    //if(i+1==index+20) index += 20;
+                    setData(intValue);
+                }
+                /*
+                if(index==20){
+                    setData(intValue);
+                    index = 0;
+                } */
+                //setData(intValue);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +71,17 @@ public class SendingSignalActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         mChart = findViewById(R.id.line_chart);
+        initChart();
 
-        initchart();
-
-
-
+        LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver, makeIntentFilter());
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(broadcastReceiver);
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(final MenuItem item) {
@@ -52,7 +93,8 @@ public class SendingSignalActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void initchart() {
+
+    private void initChart() {
         // Grid背景色
         mChart.setDrawGridBackground(true);
 
@@ -76,58 +118,63 @@ public class SendingSignalActivity extends AppCompatActivity {
         mChart.getAxisRight().setEnabled(false);
 
         // add data
-        setData();
-        setData();
+        //setData();
 
-        mChart.animateX(2500);
+        //mChart.animateX(25);
         //mChart.invalidate();
 
         // dont forget to refresh the drawing
         // mChart.invalidate();
     }
 
-    private void setData() {
+    private void setData(int intValue) {
         // Entry()を使ってLineDataSetに設定できる形に変更してarrayを新しく作成
-        int data[] = {116, 111, 112, 121, 102, 83,
+        /*int data[] = {116, 111, 112, 121, 102, 83,
                 99, 101, 74, 105, 120, 112,
                 109, 102, 107, 93, 82, 99, 110,
-        };
-
+        }; */
+        /*
         ArrayList<Entry> values = new ArrayList<>();
 
-        for (int i = 0; i < data.length; i++) {
-            values.add(new Entry(i, data[i], null, null));
-        }
+        for (int i = 0; i < 20; i++) {
+            values.add(new Entry(i, intValue[i], null, null));
+        } */
 
         LineDataSet set1;
+        //ILineDataSet set1 = mChart.getData().getDataSetByIndex(0);
 
         if (mChart.getData() != null &&
                 mChart.getData().getDataSetCount() > 0) {
 
-            set1 = (LineDataSet) mChart.getData().getDataSetByIndex(0);
-            set1.setValues(values);
+            ILineDataSet set2 = mChart.getData().getDataSetByIndex(0);
+            //set1 = (LineDataSet) mChart.getData().getDataSetByIndex(0);
+            //set1.setValues(values);
+            //set1.notifyDataSetChanged();
+            mChart.getData().addEntry(new Entry(set2.getEntryCount(), (float)intValue), 0);
+
             mChart.getData().notifyDataChanged();
             mChart.notifyDataSetChanged();
-            mChart.setVisibleXRangeMaximum(19);
+            mChart.setVisibleXRangeMaximum(800);
             mChart.moveViewToX(mChart.getData().getEntryCount());
 
+
         } else {
+            //LineData set1 = (LineData) mChart.getData().getDataSetByIndex(0);
             // create a dataset and give it a type
-            set1 = new LineDataSet(values, "DataSet");
-
-            set1.setDrawIcons(false);
+            set1 = new LineDataSet(null, "DataSet");
+            //set1.setDrawIcons(false);
             set1.setColor(Color.BLACK);
-            set1.setCircleColor(Color.BLACK);
-            set1.setLineWidth(1f);
-            set1.setCircleRadius(3f);
-            set1.setDrawCircleHole(false);
-            set1.setValueTextSize(0f);
-            set1.setDrawFilled(true);
-            set1.setFormLineWidth(1f);
-            set1.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
-            set1.setFormSize(15.f);
+            //set1.setCircleColor(Color.BLACK);
+            //set1.setLineWidth(1f);
+            //set1.setCircleRadius(3f);
+            //set1.setDrawCircleHole(false);
+            //set1.setValueTextSize(0f);
+            //set1.setDrawFilled(true);
+            //set1.setFormLineWidth(1f);
+            //set1.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
+            //set1.setFormSize(15.f);
 
-            set1.setFillColor(Color.BLUE);
+            //set1.setFillColor(Color.BLUE);
 
             ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
             dataSets.add(set1); // add the datasets
@@ -138,5 +185,13 @@ public class SendingSignalActivity extends AppCompatActivity {
             // set data
             mChart.setData(lineData);
         }
+    }
+
+
+
+    private static IntentFilter makeIntentFilter() {
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(TemplateService.BROADCAST_TEMPLATE_MEASUREMENT);
+        return intentFilter;
     }
 }
